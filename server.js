85115b2c94c99bd6c23c5b317220bfef8e0b8dc9
unknown <mosa-rko@windowslive.com>
2021-06-01 16:43:59 +0300

    connectiongmongdb

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const path = require('path')
 const api = require('./server/routes/api')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/weather-appDB', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost/weather-appDB', { useNewUrlParser: true })
 
 const app = express()
 
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
-app.use(express.json())
+app.use(exnopress.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/', api)
 
 const port = 3001
-app.listen(port, function () {
+app.listen(process.env.Port || port, function () {
     console.log("server is listening on port " + port)
 })
 
